c6f7942e3ab5fea8db1cfe1b880f8d4a9df51ca8
Gonciarov <gonciarov@yandex.ru>
2022-10-08 18:46:09 +0100

    play

---
+++ src/components/Play.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Play extends React.Component {
+    render () {
+        return (
+            <h1>Hello from Play component</h1>
+        )
+    }
+}
+
+export default Play;
