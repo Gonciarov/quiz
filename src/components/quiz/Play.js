d27ae202a1e1eb4719214ee96808bee148bcf93d
Student account <gonciarov@yandex.ru>
2022-10-10 15:48:49 +0100

    initial question added

--- src/components/quiz/Play.js
+++ src/components/quiz/Play.js
@@ -1,27 +1,46 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, Component} from 'react';
 import {Helmet} from 'react-helmet';
-import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCube } from '@fortawesome/free-solid-svg-icons';
 
 
 
-
-class Play extends React.component {
+class Play extends React.Component {
     constructor (props) {
-        super();
+        super(props);
         this.state = {
             counter: 0
         }
     };
 
+    addCount = () => {
+        this.setState({
+            counter: 5
+        })
+    }
 
    render() {
        return (
    <Fragment>
-    <Helmet><title>Quiz App - Instructions</title></Helmet>
-        <div id="instructions">      
-   <h1>Counter: {this.state.counter}</h1>
-            <Link className='back-button' to="/">Back</Link>
-       </div>
+    <Helmet><title>Quiz Page</title></Helmet>
+        <div className="questions">
+            <div className="lifeline-container">
+            <FontAwesomeIcon className="cube" icon={faCube} />
+            </div>
+            <h3>Google was founded in what year?</h3>
+            <div className="options-container">
+                <p className = "option">1997</p>
+                <p className = "option">1998</p>
+                <p className = "option">1999</p>
+                <p className = "option">2000</p>
+            </div>
+        </div>
+        <div className="button-container">
+            <button>Previous</button>
+            <button>Next</button>
+            <button>Quit</button>
+        </div>
+       
    </Fragment>
        );
    };
